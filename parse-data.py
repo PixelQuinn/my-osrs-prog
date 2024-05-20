0500472ac308d6d518d37433e16fd1b5a552d8de
Quinn Reams <quinn.reams@icloud.com>
2024-05-20 17:16:27 -0400

    fix: data parser fixed to handle unranked fields

--- parse-data.py
+++ parse-data.py
@@ -1,28 +1,32 @@
 import pandas as pd
 from io import StringIO
 
-
 def parse_player_data(raw_data):
-    data = StringIO(raw_data)
-    df = pd.read_csv(data, header=None)
-    df.columns = [
-        "Overall Rank", "Overall Level", "Attack Rank", "Attack Level", "Attack XP", "Defensive Rank", "Defensive Level",
-        "Defensive XP", "Strength Rank", "Strength Level", "Strength XP", "Hitpoints Rank", "Hitpoints Level",
-        "Hitpoints XP", "Ranged Rank", "Ranged Level", "Ranged XP", "Prayer Rank", "Prayer Level", "Prayer XP",
-        "Magic Rank", "Magic Level", "Magic XP", "Cooking Rank", "Cooking Level", "Cooking XP", "Woodcutting Rank",
-        "Woodcutting Level", "Woodcutting XP", "Fletching Rank", "Fletching Level", "Fletching XP", "Fishing Rank",
-        "Fishing Level", "Fishing XP", "Firemaking Rank", "Firemaking Level", "Firemaking XP", "Crafting Rank",
-        "Crafting Level", "Crafting XP", "Smithing Rank", "Smithing Level", "Smithing XP", "Mining Rank", "Mining Level",
-        "Mining XP", "Herblore Rank", "Herblore Level", "Herblore XP", "Agility Rank", "Agility Level", "Agility XP",
-        "Thieving Rank", "Thieving Level", "Thieving XP", "Slayer Rank", "Slayer Level", "Slayer XP", "Farming Rank",
-        "Farming Level", "Farming XP", "Runecraft Rank", "Runecraft Level", "Runecraft XP", "Huntering Rank",
-        "Hunter Level", "Hunter XP", "Construction Rank", "Construction Level", "Construction XP"
+    skills = [
+        "Overall", "Attack", "Defence", "Strength", "Hitpoints", "Ranged", "Prayer", "Magic",
+        "Cooking", "Woodcutting", "Fletching", "Fishing", "Firemaking", "Crafting",
+        "Smithing", "Mining", "Herblore", "Agility", "Thieving", "Slayer", "Farming",
+        "Runecrafting", "Hunter", "Construction"
     ]
+
+    data = StringIO(raw_data)
+    df = pd.read_csv(data, header=None, names=["Rank", "Level", "XP"])
+
+    if len(df) < len(skills):
+        missing_skills_count = len(skills) - len(df)
+        missing_data = pd.DataFrame({
+            "Rank": [None] * missing_skills_count,
+            "Level": ["Unranked"] * missing_skills_count,
+            "XP": [None] * missing_skills_count
+        })
+        df = pd.concat([df, missing_data], ignore_index=True)
+
+    df.index = skills
     return df
 
 
-# Sample date for testing
-# sample_data = "1, 99, 13034431\n2, 99, 13034431\n"
+# Sample data to test
+sample_data = "1,99,13034431\n2,99,13034431\n"
 
-# parsed_data = parse_player_data(sample_data)
-# print(parsed_data)
+parsed_data = parse_player_data(sample_data)
+print(parsed_data)
